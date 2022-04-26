c9578c6b8f6dbfc5799d7da41a7365595c365c3b
Anat94 <anatole.babin@epitech.eu>
2022-04-27 00:01:42 +0200

    [ADD] Connection system

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ let express = require('express');
 let app = express();
 let connection = require('./config.js');
 
-
 app.set('view engine', 'ejs');
 
 app.use('/assets', express.static('public'));
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-
 
 app.get('/', function(req, res) {
     res.render('pages/index.ejs');
@@ -19,7 +17,31 @@ app.post('/sign_up', function(req, res) {
 });
 
 app.post('/sign_in', function(req, res) {
-    res.render('pages/index.ejs');
+    res.redirect('/');
+});
+
+app.post('/account', function(req, res) {
+    let email = req.body.email;
+    let password = req.body.password;
+    if (email) {
+        if (password) {
+            connection.query('SELECT * FROM Utilisateurs WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+                if (error) throw error;
+                if (results.length > 0) {
+                    res.redirect('pages/account.ejs');
+                } else {
+                    err_msg = "Mot de passe incorrect";
+                    res.render('pages/index.ejs', { err_msg: err_msg } );
+                }
+            });
+        } else {
+            err_msg = "Merci de rentrer un mot de passe";
+            res.render('pages/index.ejs', { err_msg: err_msg } );
+        }
+    } else {
+        err_msg = "Merci de rentrer un email";
+        res.render('pages/index.ejs', { err_msg: err_msg } );
+    }
 });
 
 app.listen(5000);
